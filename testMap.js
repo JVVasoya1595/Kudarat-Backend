47d2495c2ed33a5c847425f3e792a695cd05a4da
JVVasoya1595 <jenilvasoya987@gmail.com>
2026-03-18 16:54:16 +0530

    delete

--- testMap.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-const Home = require('./models/Home');
-
-mongoose.connect(process.env.MONGO_URI).then(async () => {
-    const home = await Home.findOne();
-    console.log("Current DB mapEmbedUrl:", home.location.info.mapEmbedUrl);
-    process.exit();
-});
